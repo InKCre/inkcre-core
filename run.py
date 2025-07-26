8e60bf007edabc7e23e012e2df34047a8b5fb644
Lan_zhijiang <lanzhijiang@Lan-zhijiangdeMac-mini.local>
2025-07-27 05:29:26 +0800

    test4

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 """Run API Service.
 """
 
-from app import api_app
+import fastapi
+
+api_app = fastapi.FastAPI(title="InKCre")
+
+@api_app.get("/heartbeat")
+def heartbeat():
+    """Check if the API is running."""
+    return {"status": "ok"}
+
+from app.business.block import BLOCK_ROUTER
+api_app.include_router(BLOCK_ROUTER)

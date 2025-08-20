b743556ffb6429203f14beb9b988048fdab12548
Lan_zhijiang <lanzhijiang@Lan-zhijiangdeMac-mini.local>
2025-08-20 23:52:18 +0800

    utils refactor,add | ref into a package \
    add datetime utils

--- app/utils/base.py
+++ app/utils/base.py
@@ -1,4 +1,7 @@
 import enum
+import aiohttp
+import ssl
+import certifi
 import pydantic
 
 
@@ -6,3 +9,6 @@ enum_serializer = pydantic.PlainSerializer(
     lambda value: value.value if isinstance(value, enum.Enum) else value,
     return_type=str,
 )
+
+def AIOHTTP_CONNECTOR_GETTER(): 
+    return aiohttp.TCPConnector(ssl=ssl.create_default_context(cafile=certifi.where()))
